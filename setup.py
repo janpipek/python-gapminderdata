7401e00bfa816c921c76a80b65849fd7c189f64e
Jan Pipek <jan.pipek@gmail.com>
2019-10-08 10:52:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
-#!/usr/bin/env python
 """
-physt
-=====
-
-P(i/y)thon h(i/y)stograms. Inspired (and based on) numpy.histogram, but designed for humans(TM) on steroids(TM).
-
-The goal is to unify different concepts of histograms as occurring in numpy, pandas, matplotlib, ROOT, etc.
-and to create one representation that is easily manipulated with from the data point of view and at the same
-time provides nice integration into IPython notebook and various plotting options.
-
-In short, whatever you want to do with histograms, physt aims to be on your side.
-
-P.S. I am looking for anyone interested in using / developing physt. You can contribute by reporting errors, implementing missing features and suggest new one.
+TODO: Write this
 """
 
 import itertools
@@ -33,7 +21,6 @@ options = dict(
     author='Jan Pipek',
     author_email='jan.pipek@gmail.com',
     url='https://github.com/janpipek/python-gapminderdata',
-    # package_data={"physt" : ["examples/*.csv"]},
     install_requires = ['request', 'pandas'],
     python_requires="~=3.6",
     extras_require = {
