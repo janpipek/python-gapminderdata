2dce4657e70df9f31714bb680d51fb2f0a97054f
Jan Pipek <jan.pipek@gmail.com>
2023-02-26 07:52:51 +0100

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ TODO: Write this
 import itertools
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 # TODO: Perhaps rename the package before registering with pypi
 #   and make it support full ddf
